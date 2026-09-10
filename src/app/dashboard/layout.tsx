7032f13bd634f19d8a45d9690df114cb91bd2b6f
Larysson Nascimento <larysson_cb@hotmail.com>
2026-09-10 06:53:26 -0300

    feat: create route for vehicles and create route makes

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <header className="h-20 border-b border-zinc-800 flex item-center">
+        <div className="container mx-auto px-10">
+          <strong>LN Cars</strong>
+        </div>
+      </header>
+      <div className="mt-10 container mx-auto px-10">{children}</div>
+    </div>
+  );
+}
---
+++ src/app/dashboard/makes/page.tsx
@@ -0,0 +1,7 @@
+export default function Makes() {
+  return (
+    <div>
+      <h1>Marcas</h1>
+    </div>
+  );
+}

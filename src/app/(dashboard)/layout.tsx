fc0e343a8d4de14888540d18cd41af5c1a5f55ff
Larysson Nascimento <larysson_cb@hotmail.com>
2026-09-10 07:41:36 -0300

    feat: improving the style of the make page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,8 +4,8 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       <header className="h-20 border-b border-zinc-800 flex item-center">
-        <div className="container mx-auto px-10">
-          <strong>LN Cars</strong>
+        <div className="container px-10 items-center flex">
+          <strong className="font-extrabold">LN Cars</strong>
         </div>
       </header>
       <div className="mt-10 container mx-auto px-10">{children}</div>
--- src/app/(dashboard)/makes/page.tsx
+++ src/app/(dashboard)/makes/page.tsx
@@ -1,7 +1,25 @@
-export default function Makes() {
+function getMakes() {
+  new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return ['Audi', 'BMW', 'BYD', 'Honda', 'Subaru'];
+}
+
+export default async function Makes() {
+  const makes = await getMakes();
+
   return (
     <div>
-      <h1>Marcas</h1>
+      <h1 className="mb-4 font-extrabold text-4xl">Marcas</h1>
+      <div className="grid grid-cols-5 gap-4">
+        {makes.map((make) => (
+          <div
+            key={make}
+            className="bg-zinc-900 border-zinc-700 border rounded-lg grid place-items-center h-80"
+          >
+            {make}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

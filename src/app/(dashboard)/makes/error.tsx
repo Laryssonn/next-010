d64700e1b6ba162fe9e5e2428e00a1db2a87ef44
Larysson Nascimento <larysson_cb@hotmail.com>
2026-09-11 06:49:25 -0300

    feat: create page "error" to makes

---
+++ src/app/(dashboard)/makes/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Erro() {
+  return (
+    <div className="flex items-center justify-center w-full">
+      <h1 className="font-extrabold text-red-400">Erro ao acessar a página</h1>
+    </div>
+  );
+}
